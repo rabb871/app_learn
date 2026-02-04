afe6709d9ebf61ebee49f847a04a2c176bc507ad
rabb871 <2894355334@qq.com>
2026-02-05 00:04:34 +0800

    商品列表页面开发
    商品列表页面开发

---
+++ goods_list/goods_list.vue
@@ -0,0 +1,103 @@
+<template>
+  <view>
+    <view class="goods-list">
+      <view class="goods-list">
+        <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+          <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+          <my-goods :goods="item"></my-goods>
+        </view>
+      </view>
+
+	  
+    </view>
+  </view>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10,
+				},
+				// 商品列表的数据
+				goodsList: [],
+				// 总数量，用来实现分页
+				total: 0,
+				// 是否正在请求数据
+				isloading: false
+			};
+		},
+		onLoad(options) {
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			
+			this.getGoodsList()
+		},
+		methods: {
+		  // 获取商品列表数据的方法
+		  async getGoodsList(cb) {
+			// 打开节流阀
+			this.isloading = true
+			// 发起请求
+			const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+			// ** 关闭节流阀
+			this.isloading = false
+			cb && cb()
+		    if (res.meta.status !== 200) return uni.$showMsg()
+		  
+		    // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+		    this.goodsList = [...this.goodsList, ...res.message.goods]
+		    this.total = res.message.total
+		  },
+		  // 点击跳转到商品详情页面
+		  gotoDetail(item) {
+		    uni.navigateTo({
+		      url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+		    })
+		  }
+
+
+		},
+		// 触底的事件
+		onReachBottom() {
+		  // 判断是否还有下一页数据
+		  if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+		
+		  // 判断是否正在请求其它数据，如果是，则不发起额外的请求
+		  if (this.isloading) return
+		
+		  this.queryObj.pagenum += 1
+		  this.getGoodsList()
+		},
+		// 下拉刷新的事件
+		onPullDownRefresh() {
+		  this.queryObj.pagenum = 1
+		  this.total = 0
+		  this.isloading = false
+		  this.goodsList = []
+		
+		  this.getGoodsList(() => uni.stopPullDownRefresh())
+		}
+
+
+
+
+
+
+	}
+</script>
+
+<style lang="scss">
+.goods-list-container {
+	padding: 20rpx;
+}
+
+	
+
+</style>
