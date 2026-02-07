ef98ac7446da3cb0c1fed9de587395e726413d07
rabb871 <2894355334@qq.com>
2026-02-08 00:18:07 +0800

    购物车商品列表页面开发
    购物车商品列表页面开发

---
+++ cart.js
@@ -0,0 +1,79 @@
+export default {
+  // 为当前模块开启命名空间
+  namespaced: true,
+
+  state: () => {
+    try {
+      const cartStr = uni.getStorageSync('cart')
+      return {
+        cart: cartStr ? JSON.parse(cartStr) : []
+      }
+    } catch (e) {
+      console.error('购物车数据解析失败:', e)
+      return {
+        cart: []
+      }
+    }
+  },
+
+  mutations: {
+    addToCart(state, goods) {
+      const findResult = state.cart.find(x => x.goods_id === goods.goods_id)
+
+      if (!findResult) {
+        state.cart.push(goods)
+      } else {
+        findResult.goods_count++
+      }
+	  
+	  this.commit('m_cart/saveToStorage')
+    },
+	
+	saveToStorage(state) {
+	   const cartStr = JSON.stringify(state.cart)
+	   console.log('保存到本地存储:', cartStr)
+	   uni.setStorageSync('cart', cartStr)
+	   console.log('验证保存结果:', uni.getStorageSync('cart'))
+	},
+	
+	// 更新购物车中商品的勾选状态
+	updateGoodsState(state, goods) {
+	  const findResult = state.cart.find(x => x.goods_id === goods.goods_id)
+	
+	  if (findResult) {
+	    findResult.goods_state = goods.goods_state
+	    this.commit('m_cart/saveToStorage')
+	  }
+	},
+	// 更新购物车中商品的数量
+	updateGoodsCount(state, goods) {
+	  const findResult = state.cart.find(x => x.goods_id === goods.goods_id)
+	
+	  if(findResult) {
+	    findResult.goods_count = goods.goods_count
+	    this.commit('m_cart/saveToStorage')
+	  }
+	},
+	// 删除对应的商品信息
+	removeGoodsById(state, goods_id) {
+	  state.cart = state.cart.filter(x => x.goods_id !== goods_id)
+	  this.commit('m_cart/saveToStorage')
+	}
+
+
+
+
+
+  },
+
+  // 模块的 getters 属性
+  getters: {
+     total(state) {
+        let c = 0
+        state.cart.forEach(goods => c += goods.goods_count)
+        return c
+     }
+  }
+
+}
+
--- my-goods/my-goods.vue
+++ my-goods/my-goods.vue
@@ -2,29 +2,39 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+      <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
       <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
     </view>
     <!-- 商品右侧信息区域 -->
     <view class="goods-item-right">
-      <!-- 商品标题 -->
       <view class="goods-name">{{goods.goods_name}}</view>
       <view class="goods-info-box">
-        <!-- 商品价格 -->
         <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+        <uni-number-box :min="1" :value="goods.goods_count" @change="numChangeHandler"></uni-number-box>
       </view>
+
+
     </view>
+	
+	
   </view>
 </template>
 
 <script>
   export default {
-    // 定义 props 属性，用来接收外界传递到当前组件的数据
     props: {
-      // 商品的信息对象
       goods: {
         type: Object,
         default: {},
       },
+	  showRadio: {
+	    type: Boolean,
+	    default: false,
+	  },
+	  showNum: {
+	        type: Boolean,
+	        default: false,
+	  },
     },
     data() {
       return {
@@ -33,11 +43,26 @@
       }
     },
 	filters: {
-  // 把数字处理为带两位小数点的数字
-  tofixed(num) {
-    return Number(num).toFixed(2)
-  }
-}
+		// 把数字处理为带两位小数点的数字
+		tofixed(num) {
+		return Number(num).toFixed(2)
+		}
+	},
+	methods: {
+	  radioClickHandler() {
+	    this.$emit('radio-change', {
+	      goods_id: this.goods.goods_id,
+	      goods_state: !this.goods.goods_state
+	    })
+	  },
+	  numChangeHandler(val) {
+	      this.$emit('num-change', {
+	        goods_id: this.goods.goods_id,
+	        goods_count: +val
+	      })
+	    }
+	}
+
 
   }
 </script>
@@ -50,6 +75,9 @@
 
     .goods-item-left {
       margin-right: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
       .goods-pic {
         width: 100px;
@@ -60,11 +88,18 @@
 
     .goods-item-right {
       display: flex;
+      flex: 1;
       flex-direction: column;
       justify-content: space-between;
 
       .goods-name {
         font-size: 13px;
+      }
+
+      .goods-info-box {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
       }
 
       .goods-price {
