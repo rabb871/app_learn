3180519ee8a24f105741d78c249668a077eddbbc
rabb871 <2894355334@qq.com>
2026-02-10 03:24:49 +0800

    登录页面开发
    登录页面开发（完结撒花）

---
+++ user.js
@@ -0,0 +1,53 @@
+export default {
+	//开启命名空间
+	namespaced: true,
+	
+	//数据
+	state: () => ({
+		address: JSON.parse(uni.getStorageSync('address') || '{}'),
+		token: uni.getStorageSync('token') || '',
+		//用户的信息对象
+		userinfo: JSON.parse(uni.getStorageSync('userinfo') || '{}'),
+		redirectInfo: null
+	}),
+	//方法
+	mutations: {
+		//收货地址
+		updateAddress(state,address) {
+			state.address = address
+			
+			this.commit('m_user/saveAddressToStorage')
+		},
+		saveAddressToStorage(state) {
+			uni.setStorageSync('address', JSON.stringify(state.address))
+		},
+		updateUserInfo(state, userinfo) {
+			state.userinfo = userinfo
+			
+			this.commit('m_user/saveUserInfoToStorage')
+		},
+		saveUserInfoToStorage(state) {
+			uni.setStorageSync('userinfo', JSON.stringify(state.userinfo))
+		},
+		updateToken(state, token) {
+			state.token = token
+			
+			this.commit('m_user/saveTokenToStorage')
+		},
+		saveTokenToStorage(state) {
+			uni.setStorageSync('token', state.token)
+		},
+		updateRedirectInfo(state, info) {
+			state.redirectInfo = info
+		}
+	},
+	
+	getters: {
+		addstr(state) {
+			if(!state.address.provinceName) return ''
+			
+			return state.address.provinceName + state.address.cityName + state.address.countyName + state.address.detailInfo
+		}
+	}
+}
+
